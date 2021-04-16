b6ffe21dfa581be93e625f07f60dd1cc436bfaef
Artem <asustransformertf300x@gmail.com>
2021-04-16 11:56:33 +0300

    Create Compare_a_Linked_Lists.cpp

---
+++ Compare_a_Linked_Lists.cpp
@@ -0,0 +1,12 @@
+bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    while(head1->next != NULL){
+        if(head1->data != head2->data){
+            return false;
+        }
+        else{
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+    }
+    return true;
+}
